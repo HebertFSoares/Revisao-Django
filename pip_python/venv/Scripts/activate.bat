09261d3939c716d584160ed790997efbd5bc8ecb
Hebert Soares <hebertsoaresof@gmail.com>
2023-03-20 23:51:35 -0300

    Recriando ambiente virtual

--- pip_python/venv/Scripts/activate.bat
+++ pip_python/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\heber\Desktop\pip_python\venv
+set VIRTUAL_ENV=C:\Users\heber\Desktop\Dicionários\Revisao-Django\pip_python\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
